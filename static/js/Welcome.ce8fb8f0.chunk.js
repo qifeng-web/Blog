e770d593232a7df2f1fc4006d56d7c3c3224363e
gaofan05 <gaofan05@kuaishou.com>
2024-03-27 15:11:31 +0800

    feat: test1.5

---
+++ static/js/Welcome.ce8fb8f0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgithub_demo=self.webpackChunkgithub_demo||[]).push([[737],{1698:(e,s,u)=>{u.r(s),u.d(s,{default:()=>h});u(5043);var d=u(579);const h=()=>(0,d.jsx)("div",{children:"123"})}}]);
+//# sourceMappingURL=Welcome.ce8fb8f0.chunk.js.map
