f4ab92aeefb486e0d3c4047a3a42e658a0701169
gaofan05 <gaofan05@kuaishou.com>
2024-03-27 15:08:15 +0800

    feat: test1.3

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
         <>
             <div>刘世杰</div>
             <img src='https://static.yximgs.com/udata/pkg/KS-IS-AVATAR-PRODUCTION/73311/1CC605519BEFC664E21A808C06F5DFF9_compressed_100'/>
-            <div>男 电商营销组</div>
+            <div>男 电商营销组王嘉尔</div>
             <Button style={{marginRight: 8}}>离职</Button>
             <Button type="primary"> 开除</Button><br></br>
             <Input placeholder='理由' style={{ width: '20%' }}/>
